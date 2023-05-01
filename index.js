d4e1dc1a5de908aa55c2466409feb6bc4205f20e
devefy-ashish <ashishhp777@gmail.com>
2023-05-01 14:38:32 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const server = express();
 const productRouter = require("./routes/product");
+const userRouter = require("./routes/User");
 
 //bodyParser
 server.use(express.json());
 server.use("/api/products", productRouter.router);
+server.use("/api/user", userRouter.router);
 
 server.listen(5000, () => {
   console.log("server started");
